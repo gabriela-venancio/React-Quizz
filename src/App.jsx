5f946cea81e6ad040a7a41d1921fa6632e064563
gabriela-venancio <gabriela.venancio.valadao@gmail.comgit statusgit config --global user.email gabriela.venancio.valadao@gmail.com>
2023-07-15 22:30:22 -0300

    subida do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <welcome/>
+      </div>
+       
+  )
+}
+
+export default App
---
+++ src/components/welcome.jsx
@@ -0,0 +1,13 @@
+
+import './welcome.css'
+const welcome = () => {
+  return (
+    <div>
+        <h2>Ola user, bem vindo!</h2>
+        <p>clique no botão para dar inicio ao quizz</p>
+        <button>Iniciar</button>
+    </div>
+  )
+}
+
+export default welcome
